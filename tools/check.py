b4ce4934542f4ad8ab3c56d204c394b99cdec1ec
Raniere Silva <raniere@ime.unicamp.br>
2015-02-07 00:46:18 -0200

    Revert changes when validating glossary

--- tools/check.py
+++ tools/check.py
@@ -461,7 +461,8 @@ class ReferencePageValidator(MarkdownValidator):
         ```definition_lists``` extension.
 
         That syntax isn't supported by the CommonMark parser, so we identify
-         terms manually."""
+        terms manually."""
+        glossary_keyword = glossary_entry[0]
         if len(glossary_entry) < 2:
             logging.error(
                 "In {0}: "
