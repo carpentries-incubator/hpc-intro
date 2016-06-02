2171fc91a3af023cd67927569de5d1e3fbbf1973
Greg Wilson <gvwilson@third-bit.com>
2016-06-02 06:40:07 -0400

    Proper use of subprocess

--- bin/markdown-ast.rb
+++ bin/markdown-ast.rb
@@ -1,9 +1,11 @@
+#!/usr/bin/env ruby
+
+# Use Kramdown parser to produce AST for Markdown document.
+
 require "kramdown"
 require "json" 
 
-abort "No input file specified" if ARGV.length == 0
-input_file = ARGV[0]
-markdown = File.read(ARGV[0])
-doc1 = Kramdown::Document.new(markdown)
-tree = doc1.to_hash_a_s_t
+markdown = STDIN.read()
+doc = Kramdown::Document.new(markdown)
+tree = doc.to_hash_a_s_t
 puts JSON.pretty_generate(tree)
