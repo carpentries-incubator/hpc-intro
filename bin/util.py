55c04232e26307edcf83eef283191646a01f7483
Greg Wilson <gvwilson@third-bit.com>
2016-06-03 08:41:08 -0400

    Checking workshop index page

--- bin/util.py
+++ bin/util.py
@@ -14,24 +14,32 @@ class Reporter(object):
         '''Check that a dictionary has an expected value.'''
 
         if key not in values:
-            self.add('{0} does not contain {1} in {2}',
-                     name, key, filename)
+            self.add(filename, '{0} does not contain {1}', name, key)
         elif values[key] != expected:
-            self.add('{0} {1} is {2} not {3} in {4}',
-                     name, key, values[key], expected, filename)
+            self.add(filename, '{0} {1} is {2} not {3}', name, key, values[key], expected)
 
 
-    def check(self, condition, fmt, *args):
+    def check(self, condition, location, fmt, *args):
         '''Append error if condition not met.'''
 
         if not condition:
-            self.add(fmt, *args)
+            self.add(location, fmt, *args)
 
 
-    def add(self, fmt, *args):
+    def add(self, location, fmt, *args):
         '''Append error unilaterally.'''
 
-        self.messages.append(fmt.format(*args))
+        if type(location) is NoneType:
+            extra = ''
+        elif type(location) is str:
+            extra = 'at {0}'.format(filename)
+        elif type(location) is tuple:
+            filename, line_number = location
+            extra = 'at {0}:{1}'.format(*location)
+        else:
+            assert False, 'Unknown location "{0}"/{1}'.format(location, type(location))
+
+        self.messages.append(fmt.format(*args) + extra)
 
 
     def report(self, stream=sys.stdout):
