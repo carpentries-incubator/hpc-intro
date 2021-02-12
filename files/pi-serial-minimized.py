541fe665b9d1d3a492eb7e474b73e8f551205693
Mike Renfro <mike+github@renf.ro>
2021-02-12 12:53:18 -0600

    Revert "Fix pi format in text"
    This reverts commit b1dfc04153263271a7d3a2e185f38b5ae82a1b02.

--- files/pi-serial-minimized.py
+++ files/pi-serial-minimized.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 import numpy as np
 import sys
-import datetime
 
 def inside_circle(total_count):
     x = np.random.uniform(size=total_count)
@@ -12,11 +11,6 @@ def inside_circle(total_count):
 
 if __name__ == '__main__':
     n_samples = int(sys.argv[1])
-    start_time = datetime.datetime.now()
     counts = inside_circle(n_samples)
     my_pi = 4.0 * counts / n_samples
-    end_time = datetime.datetime.now()
-    elapsed_time = (end_time - start_time).total_seconds()
-    size_of_float = np.dtype(np.float64).itemsize
-    memory_required = 3 * n_samples * size_of_float / (1024**2)
-    print("Pi: {}, memory: {} MiB, time: {} s".format(my_pi, memory_required, elapsed_time))
+    print(my_pi)
