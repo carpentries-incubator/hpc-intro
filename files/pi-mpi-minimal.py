9ffbd25c238bdd97805e40af7dc6630a06a2dd36
Mike Renfro <mike+github@renf.ro>
2021-02-13 10:58:44 -0600

    Further minimizing pi-mpi-minimal.py

--- files/pi-mpi-minimal.py
+++ files/pi-mpi-minimal.py
@@ -1,12 +1,11 @@
 #!/usr/bin/env python3
 import numpy as np
 import sys
-import datetime
 from mpi4py import MPI
 
 def inside_circle(total_count):
-    x = np.float64(np.random.uniform(size=total_count))
-    y = np.float64(np.random.uniform(size=total_count))
+    x = np.random.uniform(size=total_count)
+    y = np.random.uniform(size=total_count)
     radii = np.sqrt(x*x + y*y)
     count = len(radii[np.where(radii<=1.0)])
     return count
@@ -15,10 +14,7 @@ if __name__ == '__main__':
     comm = MPI.COMM_WORLD
     cpus = comm.Get_size()
     rank = comm.Get_rank()
-    if len(sys.argv) > 1:
-        n_samples = int(sys.argv[1])
-    else:
-        n_samples = 8738128 # trust me, this number is not random :-)
+    n_samples = int(sys.argv[1])
     if rank == 0:
         partitions = [ int(n_samples / cpus) ] * cpus
         counts = [ int(0) ] * cpus
@@ -31,4 +27,4 @@ if __name__ == '__main__':
     counts = comm.gather(count_item, root=0)
     if rank == 0:
         my_pi = 4.0 * sum(counts) / n_samples
-        print("{}".format(my_pi))
+        print(my_pi)
